99813fafda958161966d4e5de5205f32c9bf3ab8
Adele Landers <adelethefish@gmail.com>
2017-11-10 12:31:38 -0800

    starting pangram

--- exercisms/javascript/simple-cipher/simple-cipher.js
+++ exercisms/javascript/simple-cipher/simple-cipher.js
@@ -1,23 +1,10 @@
+var alpha = "abcdefghijklmnopqrstuvwxyz".split("");
+
 var Cipher = function(key) {
   this.key = key;
 };
 
-Cipher.prototype.encode = function(string) {
-  var answer = [];
-  var alpha = "abcdefghijklmnopqrstuvwxyz";
-  for (var i = 0; i < string.length; i++) {
-    for (var j = 0; j < alpha.length; j++) {
-      if (alpha[i] === string[i]) {
-        answer.push(alpha[i + 3]);
-        break;
-      } else {
-        j++;
-      }
-    }
-    i++;
-  }
-  return answer.join("");
-};
+Cipher.prototype.encode = function(string) {};
 
 Cipher.prototype.decode = function() {};
 
