603981a484f23cec3256b91d897390c77e8a74b7
Adele Landers <adelethefish@gmail.com>
2017-11-13 12:06:29 -0800

    working on cipher

--- exercisms/javascript/simple-cipher/simple-cipher.js
+++ exercisms/javascript/simple-cipher/simple-cipher.js
@@ -1,11 +1,20 @@
 var alpha = "abcdefghijklmnopqrstuvwxyz".split("");
 
 var Cipher = function(key) {
-  this.key = key;
+  this.key = key || generateKey();
 };
 
-Cipher.prototype.encode = function(string) {};
+Cipher.prototype.encode = function() {};
 
 Cipher.prototype.decode = function() {};
+
+var generateKey = function() {
+  var random = "";
+  for (var i = 0; i < 10; i++) {
+    randomChar = alpha[Math.floor(Math.random() * (122 - 97 + 97))];
+    random += randomChar;
+  }
+  return random;
+};
 
 module.exports = Cipher;
