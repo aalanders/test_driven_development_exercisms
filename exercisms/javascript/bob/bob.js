1dfa483e7b225533d93c68a7d37c2f739b556c3e
Adele Landers <adelethefish@gmail.com>
2017-11-28 08:27:27 -0800

    bob

--- exercisms/javascript/bob/bob.js
+++ exercisms/javascript/bob/bob.js
@@ -3,18 +3,43 @@ var Bob = function(input) {
 };
 
 Bob.prototype.hey = function(string) {
+    var numbers = /^[0-9][,]*/.test(string)
+    //silence
+    if(string.split('').join('').length === 0){
+        return "Fine. Be that way!"
+    }
+    if(string.split(' ').join('').length === 0){
+        return "Fine. Be that way!"
+    }
+    //if numbers and ending in ?
+    if(numbers === true && string[string.length-1] === "?"){
+        return "Sure."
+    }
+    //if numbers and ending in ?
+    if(numbers === true && string[string.length-1] === "?"){
+        return "Sure."
+    }
+    //if ending in ? and capitalized
     if (string[string.length - 1] === "?" && string === string.toUpperCase()) {
       return "Whoa, chill out!";
     }
-    if (string === string.toUpperCase()){
-        return "Whoa, chill out!";
-    }
+    //if ending in ?
     if(string[string.length-1] === "?"){
         return "Sure.";
     }
+    //if capitalized and ending in !
     if (string === string.toUpperCase() && string[string.length - 1] === "!") {
        return "Whoa, chill out!";
     }
+    //if only numbers
+    if (numbers === true) {
+      return "Whatever.";
+    }
+    //if yelling
+    if (string === string.toUpperCase()){
+      return "Whoa, chill out!";
+    }
+    //if exclamation point at end
     if (string[string.length - 1] === "!") {
       return "Whatever.";;
     }
